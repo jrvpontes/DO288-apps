1689527be11e600aefe53f79e23e5f75b7d64283
Joao Pontes <jpontes@redhat.com>
2024-08-26 16:58:44 -0300

    ajustando jars.

--- builds-applications/apps/vertx-site/oc-new-app.sh
+++ builds-applications/apps/vertx-site/oc-new-app.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 oc new-app --name vertx-site \
-  --build-env MAVEN_MIRROR_URL=http://nexus-infra.apps.ocp4.example.com/repository/java \
+  --env JAVA_APP_JAR=vertx-site-1.0.0-SNAPSHOT-fat.jar \
   -i redhat-openjdk18-openshift:1.8 \
   --context-dir builds-applications/apps/vertx-site \
   https://github.com/jrvpontes/DO288-apps
