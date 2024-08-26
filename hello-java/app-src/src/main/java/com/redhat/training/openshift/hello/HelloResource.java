a998595dc22e93a470dae0895cc8775c1d31d508
Joao Pontes <jpontes@redhat.com>
2024-08-26 17:25:20 -0300

    teste

--- hello-java/app-src/src/main/java/com/redhat/training/openshift/hello/HelloResource.java
+++ hello-java/app-src/src/main/java/com/redhat/training/openshift/hello/HelloResource.java
@@ -1,6 +1,10 @@
 package com.redhat.training.openshift.hello;
 
-import javax.inject.Inject;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,19 +12,49 @@ import javax.ws.rs.Produces;
 @Path("/")
 public class HelloResource {
 
-    @GET
-    @Path("/hello")
-    @Produces("text/plain")
-    public String hello() {
-        String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
-	      String message = System.getenv().getOrDefault("APP_MSG", null);
-	      String response = "";
+	@GET
+	@Path("/hello")
+	@Produces("text/plain")
+	public String hello() {
+		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+		String message = System.getenv().getOrDefault("APP_MSG", null);
+		String response = "";
 
-      	if (message == null)
-      	  response = "Hello world from host "+hostname+"\n";
-      	else
-      	  response = "Hello world from host ["+hostname+"]. Message received = "+message+"\n";
+		if (message == null)
+			response = "Hello world from host " + hostname + "\n";
+		else
+			response = "Hello world from host [" + hostname + "]. Message received = " + message + "\n";
 
-        return response;
-    }
+		return response;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@GET
+	@Path("/env")
+	@Produces("text/plain")
+	public String env() {
+		StringBuilder response = new StringBuilder();
+		Set getenv = System.getenv().entrySet();
+		append(response,"System-Env",getenv);
+		Set getProps = System.getProperties().entrySet();
+		append(response,"System-Props",getProps);
+		
+		return response.toString();
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void append(StringBuilder response, String string, Set set) {
+		response.append("-------------------------------------------------------------------------\n");
+		response.append(string + "\n");
+		response.append("-------------------------------------------------------------------------\n");
+		Iterator<Entry> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			Entry entry = iterator.next();
+			String key = (String) entry.getKey();
+			String value = (String) entry.getValue();
+			response.append(key +"=" + value + "\n");
+		}
+	}
+
+
 }
--- hello-java/deploy-release.sh
+++ hello-java/deploy-release.sh
@@ -1,3 +1,4 @@
 mvn clean install -f app-src/
 mkdir -p ../releases/download/OCP-4.1-1/
 cp -v app-src/target/hello-thorntail.jar ../releases/download/OCP-4.1-1/hello-java.jar 
+podman build -t teste -f Containerfile
