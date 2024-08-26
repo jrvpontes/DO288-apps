7ca172786a83b78823c9bc568ba197c71563199a
Joao Pontes <jpontes@redhat.com>
2024-08-26 17:35:47 -0300

    Ajuste: 17:35:47

--- hello-java/deploy-release.sh
+++ hello-java/deploy-release.sh
@@ -1,3 +1,8 @@
+BASEDIR=$(dirname "$0")
+now=$(date +"%T")
+echo "Base dir : $BASEDIR" 
+echo "Current time Master : $now" >> "$BASEDIR/app-src/src/main/java/master.txt"
+
 mvn clean install -f app-src/
 mkdir -p ../releases/download/OCP-4.1-1/
 cp -v app-src/target/hello-thorntail.jar ../releases/download/OCP-4.1-1/hello-java.jar 
