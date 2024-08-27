74000cb78435b614334be7d480a7391d243a61c8
Joao Pontes <jpontes@redhat.com>
2024-08-26 21:13:28 -0300

    Ajuste: 21:13:28

--- micro-java/cmd_git_commit.sh
+++ micro-java/cmd_git_commit.sh
@@ -1,7 +1,7 @@
 BASEDIR=$(dirname "$0")
 now=$(date +"%T")
 echo "Base dir : $BASEDIR" 
-echo "Current time Master : $now" >> "$BASEDIR/app-src/src/main/resources/master.txt"
+echo "Current time Master : $now" >> "$BASEDIR/app-source/src/main/resources/master.txt"
 git add --force ../releases/download/OCP-4.1-1/micro-java.jar
 git -C $BASEDIR add .
 git -C $BASEDIR commit -m "Ajuste: $now"
